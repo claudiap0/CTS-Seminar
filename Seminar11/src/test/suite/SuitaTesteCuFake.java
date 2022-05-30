548e3327792e096d8c5446ee4c4113663ccee211
DESKTOP-I17P67N\Claudia <pistolclaudia@yahoo.com>
2022-05-30 15:59:46 +0300

    commit

--- Seminar11/src/test/suite/SuitaTesteCuFake.java
+++ Seminar11/src/test/suite/SuitaTesteCuFake.java
@@ -7,11 +7,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import test.TestAgentieTurism;
 import test.TestPachetTuristic;
+import test.categorii.ITesteBoundary;
 import test.categorii.ITesteCuFake;
+import test.categorii.ITestePoateRezerva;
 
 @RunWith(Categories.class)
 @Suite.SuiteClasses({TestAgentieTurism.class, TestPachetTuristic.class})
 @Categories.IncludeCategory(ITesteCuFake.class)
+@Categories.ExcludeCategory({ITesteBoundary.class, ITestePoateRezerva.class})
 public class SuitaTesteCuFake {
 
 }
